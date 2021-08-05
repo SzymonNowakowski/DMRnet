c58a158e8d8205e7364a21c0f31dc5766ebc5b16
snowakowski <szymon.nowakowski@gmail.com>
2021-08-05 14:41:27 +0200

    preparing for parallel run

--- insurance.R
+++ insurance.R
@@ -13,7 +13,7 @@ library(scriptName)
 
 
 filename_and_dirs <- current_filename()
-part_filename_and_number <- substr(filename_and_dirs, nchar(filename_and_dirs)-10, nchar(filename_and_dirs))
+part_filename_and_number <- substr(filename_and_dirs, nchar(filename_and_dirs)-11, nchar(filename_and_dirs))
 print(part_filename_and_number)
 set.seed(strtoi(substr(digest(part_filename_and_number, "md5", serialize = FALSE),1,7),16))
 cat("seed set as md5 hash of the following string: ", part_filename_and_number,"\n\n")
