2acc05262e08f8f7c89aef688608d986f6722169
snowakowski <szymon.nowakowski@gmail.com>
2021-08-09 17:43:56 +0200

    further split of computations

--- insurance_non_DMRnet_lr.R
+++ insurance_non_DMRnet_lr.R
@@ -2,10 +2,10 @@ library(digest)
 
 
 
-run_list = c("scope")
+run_list = c("lr")
 runs<-1000
 
-part_filename_and_number = "insurance2"
+part_filename_and_number = "insurance3"
 print(part_filename_and_number)
 set.seed(strtoi(substr(digest(part_filename_and_number, "md5", serialize = FALSE),1,7),16))
 cat("seed set as md5 hash of the following string: ", part_filename_and_number,"\n\n")
