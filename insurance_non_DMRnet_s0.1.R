20a64216ae21831e11a6555316ccf68929508aaf
snowakowski <szymon.nowakowski@gmail.com>
2021-08-13 17:28:29 +0200

    seed updated

--- insurance_non_DMRnet_s0.1.R
+++ insurance_non_DMRnet_s0.1.R
@@ -7,7 +7,7 @@ run_list = c("scope", "scope", "lr", "cv.glmnet")
 runs<-1000
 sigma<-0.1
 
-part_filename_and_number = "insurance_sigma=0.1"
+part_filename_and_number = "insurance_s=0.1"
 print(part_filename_and_number)
 set.seed(strtoi(substr(digest(part_filename_and_number, "md5", serialize = FALSE),1,7),16))
 cat("seed set as md5 hash of the following string: ", part_filename_and_number,"\n\n")
