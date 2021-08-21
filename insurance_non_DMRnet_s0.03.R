2f70f6e0dc8b3ae6cd53d6b20f9015f4e8e63253
SzymonNowakowski <szymon.nowakowski@gmail.com>
2021-08-21 09:07:17 +0200

    new seed as scope-32 segfaults

--- insurance_non_DMRnet_s0.03.R
+++ insurance_non_DMRnet_s0.03.R
@@ -7,7 +7,7 @@ run_list = c("scope", "scope", "lr", "cv.glmnet")
 runs<-1000
 sigma<-0.03
 
-part_filename_and_number = "insurance_s=0.03"
+part_filename_and_number = "insurance_s_0.03"
 print(part_filename_and_number)
 set.seed(strtoi(substr(digest(part_filename_and_number, "md5", serialize = FALSE),1,7),16))
 cat("seed set as md5 hash of the following string: ", part_filename_and_number,"\n\n")
