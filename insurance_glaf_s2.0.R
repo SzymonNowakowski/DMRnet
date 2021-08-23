7d0dd8db4895626aae992f191b0f7fa3c3586543
snowakowski <szymon.nowakowski@gmail.com>
2021-08-23 23:53:35 +0200

    new scripts for sigma = 2.0 in Insurance

---
+++ insurance_glaf_s2.0.R
@@ -0,0 +1,18 @@
+library(digest)
+library(scriptName)
+library(DMRnet)  #predict functions, gic from DMRnet work with GLAF models too
+
+run_list = c( "cv.GLAF", "gic.GLAF")
+runs<-50
+sigma<-2.0
+
+filename_and_dirs <- current_filename()
+part_filename_and_number <- substr(filename_and_dirs, nchar(filename_and_dirs)-21, nchar(filename_and_dirs))
+print(part_filename_and_number)
+set.seed(strtoi(substr(digest(part_filename_and_number, "md5", serialize = FALSE),1,7),16))
+cat("seed set as md5 hash of the following string: ", part_filename_and_number,"\n\n")
+cat("search for the result files postfixed with the same string: ", part_filename_and_number,"\n\n")
+
+source("glaf_4lm.R")
+
+source("insurance.R")
