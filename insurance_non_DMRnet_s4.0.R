be44400432920e8d5b982e7c8f0884ec50b90f65
snowakowski <szymon.nowakowski@gmail.com>
2021-08-24 00:01:57 +0200

    new scripts for sigma = 4.0 in Insurance

---
+++ insurance_non_DMRnet_s4.0.R
@@ -0,0 +1,18 @@
+library(digest)
+
+
+
+
+run_list = c("scope", "scope", "lr", "cv.glmnet")
+runs<-1000
+sigma<-4.0
+
+part_filename_and_number = "insurance_s=4.0"
+print(part_filename_and_number)
+set.seed(strtoi(substr(digest(part_filename_and_number, "md5", serialize = FALSE),1,7),16))
+cat("seed set as md5 hash of the following string: ", part_filename_and_number,"\n\n")
+cat("search for the result files postfixed with the same string: ", part_filename_and_number,"\n\n")
+
+
+
+source("insurance.R")
