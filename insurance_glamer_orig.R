243e1bf87a0a1b181c7d7f4fefecfccb3eceb926
snowakowski <szymon.nowakowski@gmail.com>
2021-09-03 01:08:07 +0200

    corrected length of insurance glamer file - name change

--- insurance_glamer_orig.R
+++ insurance_glamer_orig.R
@@ -8,7 +8,7 @@ run_list = c( "cp.GLAMER", "cv.GLAMER", "gic.GLAMER")
 runs<-5
 
 filename_and_dirs <- current_filename()
-part_filename_and_number <- substr(filename_and_dirs, nchar(filename_and_dirs)-21, nchar(filename_and_dirs))
+part_filename_and_number <- substr(filename_and_dirs, nchar(filename_and_dirs)-23, nchar(filename_and_dirs))
 print(part_filename_and_number)
 set.seed(strtoi(substr(digest(part_filename_and_number, "md5", serialize = FALSE),1,7),16))
 cat("seed set as md5 hash of the following string: ", part_filename_and_number,"\n\n")
