9207e5e1322d87476b543d65984aace251c0826f
SzymonNowakowski <73636503+SzymonNowakowski@users.noreply.github.com>
2021-09-02 23:58:26 +0200

    heights calculation corrected, although it is not needed for now

--- glamer_4lm.R
+++ glamer_4lm.R
@@ -302,10 +302,13 @@ glamer_4lm <- function(X, y, clust.method = "complete", lambda = NULL, nlambda =
     }
     model_group <- function(i) {ind[i]}
     model_index_within_group <- function(i) {i - sum(rss[, model_group(i)] == Inf)}
-    model_index_within_group_inverted <- function(i) {length(mm[[model_group(i)]]$heights)-model_index_within_group(i)+1}
+    #model_index_within_group_inverted <- function(i) {length(mm[[model_group(i)]]$heights)-model_index_within_group(i)+1}
 
     be <- sapply(idx, function(i) {b_matrix<-mm[[model_group(i)]]$b; if (is.null(dim(b_matrix))) b_matrix<-matrix(b_matrix); part2beta_4lm_help(b = b_matrix[, model_index_within_group(i)], S = SS[, model_group(i)], X = X, y = y, fl=fl)})
-    heights <- sapply(length(idx):1, function(i) mm[[model_group(i)]]$heights[model_index_within_group_inverted(i)])
+    
+    heights <- sapply(idx, function(i) mm[[model_group(i)]]$heights[model_index_within_group(i)])
+    #heights from full model #1 with height == 0 to the last 1-element model with height > 0
+                      
     rownames(be) <- colnames(x.full)
     if(length(ord) > 0){
                   ind1 <- c(1:p)
