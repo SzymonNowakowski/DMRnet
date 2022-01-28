3d1fd393304f0fd31d30d721ff7c64f84e4b708b
snowakowski <szymon.nowakowski@gmail.com>
2022-01-28 17:09:18 +0100

    new tests for single 2-level factor column in gaussian family

--- hard_case_DMRnet.R
+++ hard_case_DMRnet.R
@@ -3,6 +3,11 @@ load_all()
 data(insXtr)
 data(insytr)
 
+print("dmrnet single 2-level factor column:")
+dmrnet <- DMRnet(Xtr[1], ytr, family = "gaussian")
+print("dmr single 2-level factor column:")
+dmr <- DMR(Xtr[1], ytr, family = "gaussian")
+
 print("dmrnet:")
 dmrnet <- DMRnet(Xtr, ytr, family = "gaussian")
 print("cv.dmrnet:")
