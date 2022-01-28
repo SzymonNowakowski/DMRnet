6f3d97f14bce403431cb521559cf631831fe60de
snowakowski <szymon.nowakowski@gmail.com>
2022-01-29 00:05:03 +0100

    added binomial family "hard tests" and also verified the result in the previous 0.2.0 package version

--- hard_case_DMRnet.R
+++ hard_case_DMRnet.R
@@ -4,16 +4,50 @@ data(insXtr)
 data(insytr)
 
 print("dmrnet single 2-level factor column:")
-dmrnet <- DMRnet(Xtr[1], ytr, family = "gaussian")
+dmrnet <- DMRnet(Xtr[1], ytr, family = "gaussian")     #fails in 0.2.0 - Error in grpreg: Error: X must be a matrix or able to be coerced to a matrix
 print("dmr single 2-level factor column:")
-dmr <- DMR(Xtr[1], ytr, family = "gaussian")
+dmr <- DMR(Xtr[1], ytr, family = "gaussian")           #passes in 0.2.0
 
 print("dmrnet:")
-dmrnet <- DMRnet(Xtr, ytr, family = "gaussian")
+dmrnet <- DMRnet(Xtr, ytr, family = "gaussian")        #fails in 0.2.0 - Error in solve.default(rX) :
+                                                                        #system is computationally singular: reciprocal condition number = 8.83141e-33
 print("cv.dmrnet:")
-cv.dmrnet  <- cv.DMRnet(Xtr, ytr, family = "gaussian")
+cv.dmrnet  <- cv.DMRnet(Xtr, ytr, family = "gaussian") #fails in 0.2.0 probably because of factor levels conflict between train and test sets - Error: Missing data (NA's) detected in X.  You must eliminate missing data (e.g., by removing cases, removing features, or imputation) before passing X to grpreg
 print("dmr:")
-dmr <- DMR(Xtr, ytr, family = "gaussian")
+dmr <- DMR(Xtr, ytr, family = "gaussian")              #fails in 0.2.0 - Error in solve.default(rX) :
+                                                                        #system is computationally singular: reciprocal condition number = 8.83141e-33
 print("cv.dmr:")
-cv.dmr <- cv.DMR(Xtr, ytr, family = "gaussian")
+cv.dmr <- cv.DMR(Xtr, ytr, family = "gaussian")        #fails in 0.2.0 probably because of factor levels conflict between train and test sets - Error in solve.default(rX) :
+                                                                                                                                               #Lapack routine dgesv: system is exactly singular: U[433,433] = 0
 
+
+
+ytr<-factor(ytr>mean(ytr))
+
+
+print("dmrnet single 2-level factor column:")
+dmrnet <- DMRnet(Xtr[1], ytr, family = "binomial")     #fails in 0.2.0 - Error in grpreg: Error: X must be a matrix or able to be coerced to a matrix
+print("dmr single 2-level factor column:")
+dmr <- DMR(Xtr[1], ytr, family = "binomial")           #passes in 0.2.0
+
+print("dmrnet:")
+dmrnet <- DMRnet(Xtr, ytr, family = "binomial")        #fails in 0.2.0 - Error in stats::cutree(models[[kt]], h = heig[i]) :
+                                                                        #invalid 'tree' ('merge' component)
+                                                                        #In addition: Warning messages:
+                                                                        #1: In DMR4glm_help(Xn, y, clust.method = clust.method, lam = lam) :
+                                                                        #  NAs introduced by coercion
+                                                                        #2: In min(sp[[kt]][sp[[kt]] != 1]) :
+                                                                        #  no non-missing arguments to min; returning Inf
+print("cv.dmrnet:")
+cv.dmrnet  <- cv.DMRnet(Xtr, ytr, family = "binomial") #fails in 0.2.0 probably because of factor levels conflict between train and test sets - Error: Missing data (NA's) detected in X.  You must eliminate missing data (e.g., by removing cases, removing features, or imputation) before passing X to grpreg
+print("dmr:")
+dmr <- DMR(Xtr, ytr, family = "binomial")              #fails in 0.2.0 - Error in stats::cutree(models[[kt]], h = heig[i]) :
+                                                                        #invalid 'tree' ('merge' component)
+                                                                        #In addition: Warning messages:
+                                                                        #1: In DMR4glm(X, y, clust.method = clust.method, lam = lam) :
+                                                                        #  NAs introduced by coercion
+                                                                        #2: In min(sp[[kt]][sp[[kt]] != 1]) :
+                                                                        #  no non-missing arguments to min; returning Inf
+print("cv.dmr:")
+cv.dmr <- cv.DMR(Xtr, ytr, family = "binomial")        #fails in 0.2.0 probably because of factor levels conflict between train and test sets - Error in stats::hclust(stats::as.dist(t(x)), method = clust.method, members = NULL) :
+                                                                                                                                             #  NA/NaN/Inf in foreign function call (arg 10)
