8ac7e49441eee84193ed327551730311bfa3f112
snowakowski <szymon.nowakowski@gmail.com>
2022-02-09 00:58:58 +0100

    understood fac variable a little bit better

--- R/DMRnet4lm.R
+++ R/DMRnet4lm.R
@@ -79,7 +79,7 @@ DMRnet4lm <- function(X, y, clust.method, o, nlambda, lam, maxp, lambda){
     ii <- duplicated(t(bb_predictor_sets))    #detecting duplicated predictor sets
     prz <- rep(1:p.x, fl-1)
     fac <- apply(bb[-1,ii == FALSE, drop = FALSE], 2, function(x) tapply(x, factor(prz), function(z) sum(z^2)*sqrt(length(z))))
-    #fac is a matrix with normalized betas relating to variables (rows) respective to non-duplicated lambdas (colums)
+    #fac is a matrix with normalized beta statistics relating to GROUPS of variables (rows) respective to non-duplicated lambdas (colums)
     if(is.null(dim(fac))){  #in case of a single 2-level factor matrix in X, there is only one beta and fac would be reduced to a vector. This line here helps to convert it back to a matrix
       #by the way, a symmetric situation is not possible as grpreg does NOT accept a single lambda value nor nlambda=1, nlambda must be at least two
        fac <- t(as.matrix(fac))
