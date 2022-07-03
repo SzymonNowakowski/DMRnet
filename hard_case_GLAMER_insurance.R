87999e9b1c3bc4e6aae693cf23f7683c11cef756
SzymonNowakowski <szymon.nowakowski@gmail.com>
2022-07-03 09:09:23 +0200

    Extracted a problem with NAs found in DMRnet, too

--- hard_case_GLAMER_insurance.R
+++ hard_case_GLAMER_insurance.R
@@ -42,6 +42,21 @@ if (is.na(sum(coef(mod, df=439)))) {    # has NA ?
 data("5_cv_MD")
 mod <- DMRnet(Xtr, ytr, algorithm="glamer")
 
+################DMRnet in CVG had problems with NAs too, on the 1st run AFTER the fix in massive_insurance (commit 4eca99d)
+#cvg.DMRnet with cvg
+#1 error =  NaN -> NaN
+#1 df =   ->
+#  Error in results_dfs[total_run, index - 1] <- df :
+#  replacement has length zero
+#Execution halted
+
+#isolated case for DMRnet (CV not necessary here)
+data(DMRnet_NaN)
+mod <- DMRnet(X, y)
+if (is.na(sum(coef(mod, df=460)))) {    # has NA ?
+  stop("Found NA values in a model returned from GLAMER")
+}
+
 
 
 
