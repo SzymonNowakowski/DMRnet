f65cff3bd91339f57977643282acc819ce22e170
SzymonNowakowski <szymon.nowakowski@gmail.com>
2022-07-06 08:55:35 +0200

    changed the output of LAPACK test

--- hard_case_LAPACK_SVD_insurance.R
+++ hard_case_LAPACK_SVD_insurance.R
@@ -22,27 +22,32 @@ glamer <- DMRnet(Xtr, ytr, family = "gaussian", algorithm = "glamer")
 
 
   ################# ISOLATED CASE ###################
+cat("isolated_case:\n")
 data(crashes_svd)
-svd(crashes_svd)    #<--------------crashes
+d<-svd(crashes_svd)    #<--------------crashes
 
 
 
 ############## GLAMER with cv+sd had problems with SVD routine on the 5th CV run on 17th run of massive-insurance tests with a seed derived from "insUrance_"
+cat("glamer:\n")
 data("5_cv_MD")
 mod <- DMRnet(Xtr, ytr, algorithm="glamer")   #Error in La.svd(x, nu, nv) : error code 1 from Lapack routine 'dgesdd'
 
   ################# ISOLATED CASE ###################
+cat("isolated_case:\n")
 data(crashes_svd_1)
-svd(crashes_svd)    #<--------------crashes
+d<-svd(crashes_svd)    #<--------------crashes
 
 
 
 ############## GLAMER with cv+sd had problems with SVD routine on the 4th CV run on 7th run of massive-insurance tests with a seed derived from "insURance_" AFTER the fix in massive_insurance (commit 4eca99d)
+cat("glamer:\n")
 data("4_cv_MD")
 mod <- DMRnet(Xtr, ytr, algorithm="glamer")   #Error in La.svd(x, nu, nv) : error code 1 from Lapack routine 'dgesdd'
 
   ################# ISOLATED CASE ###################
+cat("isolated_case:\n")
 data(crashes_svd_2)
-svd(crashes_svd)    #<--------------crashes
+d<-svd(crashes_svd)    #<--------------crashes
 
 cat("completed\n")
