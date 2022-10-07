fe7fcb7e31d78016c9b97216c82ca5a78b8b1087
SzymonNowakowski <szymon.nowakowski@gmail.com>
2022-10-08 00:32:59 +0200

    fixing the plot for 1se model in GIC-indexed CV if Inf value present

--- R/plot.cv.DMR.R
+++ R/plot.cv.DMR.R
@@ -25,7 +25,7 @@ plot.cv.DMR <- function(x, ...){
   graphics::text(x$df.min, min(stats::na.omit(x$cvm)), "df.min", pos=3, cex=0.7, col = "red")
 
   #1 SD line, arrow and text
-  y <- min(stats::na.omit(x$cvm)) + stats::sd(stats::na.omit(x$cvm))
+  y <- min(stats::na.omit(x$cvm)) + stats::sd(stats::na.omit(x$cvm[x$cvm!=Inf & x$cvm!=-Inf]))
   graphics::lines(c(length(x$cvm),1), c(y, y), lty="dashed", col = "blue")
   graphics::arrows(1, min(stats::na.omit(x$cvm)), 1, y, length=0.05, code=3)
   graphics::text(1, y/2+min(stats::na.omit(x$cvm))/2, "1SD", pos=4, cex=0.7)
