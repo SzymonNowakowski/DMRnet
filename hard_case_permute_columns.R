72a84608c6b8648428d24e1e8c9ab2661c223730
SzymonNowakowski <szymon.nowakowski@gmail.com>
2022-10-31 16:40:31 +0100

    adding more scenarios, with intercept too

--- hard_case_permute_columns.R
+++ hard_case_permute_columns.R
@@ -1,8 +1,128 @@
 library(devtools)
 load_all()
 
-######### SCENARIO A
+stopper <- FALSE
 
+###############################################################
+###### Problems with constant intercept columns ###############
+###############################################################
+
+
+
+######### SCENARIO A SOSnet no intercept, matrix with interecept
+
+cat("Sosnet\n")
+X <- matrix(rnorm(50*15), ncol=15)
+X[,1] <- 1.0    #constant
+b_vector <- sample(1:10,15, replace=TRUE)
+y <- X %*% matrix(b_vector)
+
+
+mod <- DMRnet(X, y, interc=FALSE)   #SOSnet in fact - no factor columns
+pred <- predict(mod, newx=X, df=14)
+MSEA <- sum((y - pred)^2)
+cat("Scenario A: ", MSEA, "\n")
+
+
+######### SCENARIO B SOSnet no intercept, matrix no interecept
+
+cat("Sosnet\n")
+X <- matrix(rnorm(50*15), ncol=15)
+b_vector <- sample(1:10,15, replace=TRUE)
+y <- X %*% matrix(b_vector)
+
+
+mod <- DMRnet(X, y, interc=FALSE)   #SOSnet in fact - no factor columns
+pred <- predict(mod, newx=X, df=15)
+MSEB <- sum((y - pred)^2)
+cat("Scenario B: ", MSEB, "\n")
+
+######### SCENARIO C SOSnet with intercept, matrix with interecept
+
+cat("Sosnet\n")
+X <- matrix(rnorm(50*15), ncol=15)
+X[,1] <- 1.0    #constant
+b_vector <- sample(1:10,15, replace=TRUE)
+y <- X %*% matrix(b_vector)
+
+
+mod <- DMRnet(X, y, interc=TRUE)   #SOSnet in fact - no factor columns
+pred <- predict(mod, newx=X, df=15)
+MSEC <- sum((y - pred)^2)
+cat("Scenario C: ", MSEA, "\n")
+
+
+######### SCENARIO B SOSnet with intercept, matrix no interecept
+
+cat("Sosnet\n")
+X <- matrix(rnorm(50*15), ncol=15)
+b_vector <- sample(1:10,15, replace=TRUE)
+y <- X %*% matrix(b_vector)
+
+
+mod <- DMRnet(X, y, interc=TRUE)   #SOSnet in fact - no factor columns
+pred <- predict(mod, newx=X, df=16)
+MSED <- sum((y - pred)^2)
+cat("Scenario D: ", MSEB, "\n")
+
+
+
+if (stopper)
+  if (MSEA / MSEB > 1000.0 | MSEA > 1e-10 | MSEB > 1e-10)
+    stop(paste("Incorrect computation for permuted columns: MSE in scenario A =", MSEA, "while MSE in scenario B =", MSEB))
+
+
+
+###############################################################
+###### Permutation of factor/numeric columns ##################
+###############################################################
+
+
+
+######### SCENARIO A DMR
+
+cat("DMR\n")
+X <- matrix(sample(1:2,750, replace=TRUE), ncol=15)
+for (i in 1:13)
+  X[,i] <- rnorm(50)
+b_vector <- sample(1:10,2, replace=TRUE)
+y <- X[,14:15] %*% matrix(b_vector)
+
+X<-data.frame(X)
+for (i in 14:15)
+  X[,i] <- factor(X[,i])
+
+mod <- DMR(X, y)
+pred <- predict(mod, newx=X, df=3)
+MSEA <- sum((y - pred)^2)
+cat("Scenario A: ", MSEA, "\n")
+
+
+######### SCENARIO B DMR
+
+X <- matrix(sample(1:2,750, replace=TRUE), ncol=15)
+for (i in 3:15)
+  X[,i] <- rnorm(50)
+b_vector <- sample(1:10,2, replace=TRUE)
+y <- X[,1:2] %*% matrix(b_vector)
+
+X<-data.frame(X)
+for (i in 1:2)
+  X[,i] <- factor(X[,i])
+
+mod <- DMR(X, y)
+pred <- predict(mod, newx=X, df=3)
+MSEB <- sum((y - pred)^2)
+cat("Scenario B: ", MSEB, "\n")
+
+if (stopper)
+  if (MSEA / MSEB > 1000.0 | MSEA > 1e-10 | MSEB > 1e-10)
+    stop(paste("Incorrect computation for permuted columns: MSE in scenario A =", MSEA, "while MSE in scenario B =", MSEB))
+
+
+######### SCENARIO A DMRnet
+
+cat("DMRnet\n")
 X <- matrix(sample(1:2,750, replace=TRUE), ncol=15)
 for (i in 1:13)
   X[,i] <- rnorm(50)
@@ -19,7 +139,7 @@ MSEA <- sum((y - pred)^2)
 cat("Scenario A: ", MSEA, "\n")
 
 
-######### SCENARIO B
+######### SCENARIO B DMRnet
 
 X <- matrix(sample(1:2,750, replace=TRUE), ncol=15)
 for (i in 3:15)
@@ -37,7 +157,53 @@ MSEB <- sum((y - pred)^2)
 cat("Scenario B: ", MSEB, "\n")
 
 
-if (MSEA / MSEB > 1000.0 | MSEA > 1e-10 | MSEB > 1e-10)
-  stop(paste("Incorrect compuation of permuted columns: MSE in scenario A =", MSEA, "while MSE in scenario B =", MSEB))
+if (stopper)
+  if (MSEA / MSEB > 1000.0 | MSEA > 1e-10 | MSEB > 1e-10)
+    stop(paste("Incorrect computation for permuted columns: MSE in scenario A =", MSEA, "while MSE in scenario B =", MSEB))
+
+
+
+######### SCENARIO A GLAMER
+
+cat("GLAMER\n")
+X <- matrix(sample(1:2,750, replace=TRUE), ncol=15)
+for (i in 1:13)
+  X[,i] <- rnorm(50)
+b_vector <- sample(1:10,2, replace=TRUE)
+y <- X[,14:15] %*% matrix(b_vector)
+
+X<-data.frame(X)
+for (i in 14:15)
+  X[,i] <- factor(X[,i])
+
+mod <- DMRnet(X, y, algorithm="glamer")
+pred <- predict(mod, newx=X, df=3)
+MSEA <- sum((y - pred)^2)
+cat("Scenario A: ", MSEA, "\n")
+
+
+######### SCENARIO B GLAMER
+
+X <- matrix(sample(1:2,750, replace=TRUE), ncol=15)
+for (i in 3:15)
+  X[,i] <- rnorm(50)
+b_vector <- sample(1:10,2, replace=TRUE)
+y <- X[,1:2] %*% matrix(b_vector)
+
+X<-data.frame(X)
+for (i in 1:2)
+  X[,i] <- factor(X[,i])
+
+mod <- DMRnet(X, y, algorithm="glamer")
+pred <- predict(mod, newx=X, df=3)
+MSEB <- sum((y - pred)^2)
+cat("Scenario B: ", MSEB, "\n")
+
+
+if (stopper)
+  if (MSEA / MSEB > 1000.0 | MSEA > 1e-10 | MSEB > 1e-10)
+    stop(paste("Incorrect compuration for permuted columns: MSE in scenario A =", MSEA, "while MSE in scenario B =", MSEB))
+
+
 
 cat("completed\n")
