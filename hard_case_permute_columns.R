8b560aef9654bac1c5d63bfc3254821e84bdf208
SzymonNowakowski <szymon.nowakowski@gmail.com>
2022-11-01 15:50:33 +0100

    adding binomial testing

--- hard_case_permute_columns.R
+++ hard_case_permute_columns.R
@@ -3,204 +3,228 @@ load_all()
 
 stopper <- FALSE
 
+
+
 ###############################################################
 ###### Problems with constant intercept columns ###############
 ###############################################################
 
+execute <- function(family) {
 
+  ######### SCENARIO A SOSnet no intercept, matrix with intercept
 
-######### SCENARIO A SOSnet no intercept, matrix with intercept
+  cat("Sosnet\n")
+  X <- matrix(rnorm(50*15), ncol=15)
+  X[,1] <- 1.0    #constant
+  b_vector <- sample(1:10,15, replace=TRUE)
+  y <- X %*% matrix(b_vector)
+  if (family == "binomial")
+    y <- factor(y>mean(y))
 
-cat("Sosnet\n")
-X <- matrix(rnorm(50*15), ncol=15)
-X[,1] <- 1.0    #constant
-b_vector <- sample(1:10,15, replace=TRUE)
-y <- X %*% matrix(b_vector)
+  mod <- DMRnet(X, y, interc=FALSE, family=family)   #SOSnet gets called - no factor columns
+  #pred <- predict(mod, newx=X, df=14)
+  MSEA <- sum((y - pred)^2)
+  cat("Scenario A: ", MSEA, "\n")
 
 
-mod <- DMRnet(X, y, interc=FALSE)   #SOSnet gets called - no factor columns
-#pred <- predict(mod, newx=X, df=14)
-MSEA <- sum((y - pred)^2)
-cat("Scenario A: ", MSEA, "\n")
+  ######### SCENARIO B SOSnet no intercept, matrix no interecept
 
+  X <- matrix(rnorm(50*15), ncol=15)
+  b_vector <- sample(1:10,15, replace=TRUE)
+  y <- X %*% matrix(b_vector)
+  if (family == "binomial")
+    y <- factor(y>mean(y))
 
-######### SCENARIO B SOSnet no intercept, matrix no interecept
 
-X <- matrix(rnorm(50*15), ncol=15)
-b_vector <- sample(1:10,15, replace=TRUE)
-y <- X %*% matrix(b_vector)
+  mod <- DMRnet(X, y, interc=FALSE, family=family)   #SOSnet gets called - no factor columns
+  pred <- predict(mod, newx=X, df=15)
+  MSEB <- sum((y - pred)^2)
+  cat("Scenario B: ", MSEB, "\n")
 
+  ######### SCENARIO C SOSnet with intercept, matrix with intercept
 
-mod <- DMRnet(X, y, interc=FALSE)   #SOSnet gets called - no factor columns
-pred <- predict(mod, newx=X, df=15)
-MSEB <- sum((y - pred)^2)
-cat("Scenario B: ", MSEB, "\n")
+  X <- matrix(rnorm(50*15), ncol=15)
+  X[,1] <- 1.0    #constant
+  b_vector <- sample(1:10,15, replace=TRUE)
+  y <- X %*% matrix(b_vector)
+  if (family == "binomial")
+    y <- factor(y>mean(y))
 
-######### SCENARIO C SOSnet with intercept, matrix with intercept
 
-X <- matrix(rnorm(50*15), ncol=15)
-X[,1] <- 1.0    #constant
-b_vector <- sample(1:10,15, replace=TRUE)
-y <- X %*% matrix(b_vector)
+  mod <- DMRnet(X, y, interc=TRUE, family=family)   #SOSnet gets called - no factor columns
+  #pred <- predict(mod, newx=X, df=15)
+  MSEC <- sum((y - pred)^2)
+  cat("Scenario C: ", MSEA, "\n")
 
 
-mod <- DMRnet(X, y, interc=TRUE)   #SOSnet gets called - no factor columns
-#pred <- predict(mod, newx=X, df=15)
-MSEC <- sum((y - pred)^2)
-cat("Scenario C: ", MSEA, "\n")
+  ######### SCENARIO D SOSnet with intercept, matrix no intercept
 
+  X <- matrix(rnorm(50*15), ncol=15)
+  b_vector <- sample(1:10,15, replace=TRUE)
+  y <- X %*% matrix(b_vector)
+  if (family == "binomial")
+    y <- factor(y>mean(y))
 
-######### SCENARIO D SOSnet with intercept, matrix no intercept
 
-X <- matrix(rnorm(50*15), ncol=15)
-b_vector <- sample(1:10,15, replace=TRUE)
-y <- X %*% matrix(b_vector)
+  mod <- DMRnet(X, y, interc=TRUE, family=family)   #SOSnet gets called - no factor columns
+  pred <- predict(mod, newx=X, df=16)
+  MSED <- sum((y - pred)^2)
+  cat("Scenario D: ", MSEB, "\n")
 
 
-mod <- DMRnet(X, y, interc=TRUE)   #SOSnet gets called - no factor columns
-pred <- predict(mod, newx=X, df=16)
-MSED <- sum((y - pred)^2)
-cat("Scenario D: ", MSEB, "\n")
 
+  if (stopper)
+    if ( MSEA > 1e-10 | MSEB > 1e-10 | MSEC > 1e-10 | MSED > 1e-10)
+      stop("Incorrect computation with/without intercept in SOSnet")
 
 
-if (stopper)
-  if ( MSEA > 1e-10 | MSEB > 1e-10 | MSEC > 1e-10 | MSED > 1e-10)
-    stop("Incorrect computation with/without intercept in SOSnet")
 
+  ###############################################################
+  ###### Permutation of factor/numeric columns ##################
+  ###############################################################
 
 
-###############################################################
-###### Permutation of factor/numeric columns ##################
-###############################################################
 
+  ######### SCENARIO A DMR
 
+  cat("DMR\n")
+  X <- matrix(sample(1:2,750, replace=TRUE), ncol=15)
+  for (i in 1:13)
+    X[,i] <- rnorm(50)
+  b_vector <- sample(1:10,2, replace=TRUE)
+  y <- X[,14:15] %*% matrix(b_vector)
+  if (family == "binomial")
+    y <- factor(y>mean(y))
 
-######### SCENARIO A DMR
+  X<-data.frame(X)
+  for (i in 14:15)
+    X[,i] <- factor(X[,i])
 
-cat("DMR\n")
-X <- matrix(sample(1:2,750, replace=TRUE), ncol=15)
-for (i in 1:13)
-  X[,i] <- rnorm(50)
-b_vector <- sample(1:10,2, replace=TRUE)
-y <- X[,14:15] %*% matrix(b_vector)
+  mod <- DMR(X, y, family=family)
+  pred <- predict(mod, newx=X, df=3)
+  MSEA <- sum((y - pred)^2)
+  cat("Scenario A: ", MSEA, "\n")
 
-X<-data.frame(X)
-for (i in 14:15)
-  X[,i] <- factor(X[,i])
 
-mod <- DMR(X, y)
-pred <- predict(mod, newx=X, df=3)
-MSEA <- sum((y - pred)^2)
-cat("Scenario A: ", MSEA, "\n")
+  ######### SCENARIO B DMR
 
+  X <- matrix(sample(1:2,750, replace=TRUE), ncol=15)
+  for (i in 3:15)
+    X[,i] <- rnorm(50)
+  b_vector <- sample(1:10,2, replace=TRUE)
+  y <- X[,1:2] %*% matrix(b_vector)
+  if (family == "binomial")
+    y <- factor(y>mean(y))
+
+  X<-data.frame(X)
+  for (i in 1:2)
+    X[,i] <- factor(X[,i])
 
-######### SCENARIO B DMR
+  mod <- DMR(X, y, family=family)
+  pred <- predict(mod, newx=X, df=3)
+  MSEB <- sum((y - pred)^2)
+  cat("Scenario B: ", MSEB, "\n")
 
-X <- matrix(sample(1:2,750, replace=TRUE), ncol=15)
-for (i in 3:15)
-  X[,i] <- rnorm(50)
-b_vector <- sample(1:10,2, replace=TRUE)
-y <- X[,1:2] %*% matrix(b_vector)
+  if (stopper)
+    if (MSEA / MSEB > 1000.0 | MSEA > 1e-10 | MSEB > 1e-10)
+      stop(paste("Incorrect computation for permuted columns: MSE in scenario A =", MSEA, "while MSE in scenario B =", MSEB))
 
-X<-data.frame(X)
-for (i in 1:2)
-  X[,i] <- factor(X[,i])
 
-mod <- DMR(X, y)
-pred <- predict(mod, newx=X, df=3)
-MSEB <- sum((y - pred)^2)
-cat("Scenario B: ", MSEB, "\n")
+  ######### SCENARIO A DMRnet
 
-if (stopper)
-  if (MSEA / MSEB > 1000.0 | MSEA > 1e-10 | MSEB > 1e-10)
-    stop(paste("Incorrect computation for permuted columns: MSE in scenario A =", MSEA, "while MSE in scenario B =", MSEB))
+  cat("DMRnet\n")
+  X <- matrix(sample(1:2,750, replace=TRUE), ncol=15)
+  for (i in 1:13)
+    X[,i] <- rnorm(50)
+  b_vector <- sample(1:10,2, replace=TRUE)
+  y <- X[,14:15] %*% matrix(b_vector)
+  if (family == "binomial")
+    y <- factor(y>mean(y))
 
+  X<-data.frame(X)
+  for (i in 14:15)
+    X[,i] <- factor(X[,i])
 
-######### SCENARIO A DMRnet
+  mod <- DMRnet(X, y, family=family)
+  pred <- predict(mod, newx=X, df=3)
+  MSEA <- sum((y - pred)^2)
+  cat("Scenario A: ", MSEA, "\n")
 
-cat("DMRnet\n")
-X <- matrix(sample(1:2,750, replace=TRUE), ncol=15)
-for (i in 1:13)
-  X[,i] <- rnorm(50)
-b_vector <- sample(1:10,2, replace=TRUE)
-y <- X[,14:15] %*% matrix(b_vector)
 
-X<-data.frame(X)
-for (i in 14:15)
-  X[,i] <- factor(X[,i])
+  ######### SCENARIO B DMRnet
 
-mod <- DMRnet(X, y)
-pred <- predict(mod, newx=X, df=3)
-MSEA <- sum((y - pred)^2)
-cat("Scenario A: ", MSEA, "\n")
+  X <- matrix(sample(1:2,750, replace=TRUE), ncol=15)
+  for (i in 3:15)
+    X[,i] <- rnorm(50)
+  b_vector <- sample(1:10,2, replace=TRUE)
+  y <- X[,1:2] %*% matrix(b_vector)
+  if (family == "binomial")
+    y <- factor(y>mean(y))
 
+  X<-data.frame(X)
+  for (i in 1:2)
+    X[,i] <- factor(X[,i])
 
-######### SCENARIO B DMRnet
+  mod <- DMRnet(X, y, family=family)
+  pred <- predict(mod, newx=X, df=3)
+  MSEB <- sum((y - pred)^2)
+  cat("Scenario B: ", MSEB, "\n")
 
-X <- matrix(sample(1:2,750, replace=TRUE), ncol=15)
-for (i in 3:15)
-  X[,i] <- rnorm(50)
-b_vector <- sample(1:10,2, replace=TRUE)
-y <- X[,1:2] %*% matrix(b_vector)
 
-X<-data.frame(X)
-for (i in 1:2)
-  X[,i] <- factor(X[,i])
+  if (stopper)
+    if (MSEA / MSEB > 1000.0 | MSEA > 1e-10 | MSEB > 1e-10)
+      stop(paste("Incorrect computation for permuted columns: MSE in scenario A =", MSEA, "while MSE in scenario B =", MSEB))
 
-mod <- DMRnet(X, y)
-pred <- predict(mod, newx=X, df=3)
-MSEB <- sum((y - pred)^2)
-cat("Scenario B: ", MSEB, "\n")
 
 
-if (stopper)
-  if (MSEA / MSEB > 1000.0 | MSEA > 1e-10 | MSEB > 1e-10)
-    stop(paste("Incorrect computation for permuted columns: MSE in scenario A =", MSEA, "while MSE in scenario B =", MSEB))
+  ######### SCENARIO A GLAMER
 
+  cat("GLAMER\n")
+  X <- matrix(sample(1:2,750, replace=TRUE), ncol=15)
+  for (i in 1:13)
+    X[,i] <- rnorm(50)
+  b_vector <- sample(1:10,2, replace=TRUE)
+  y <- X[,14:15] %*% matrix(b_vector)
+  if (family == "binomial")
+    y <- factor(y>mean(y))
 
+  X<-data.frame(X)
+  for (i in 14:15)
+    X[,i] <- factor(X[,i])
 
-######### SCENARIO A GLAMER
+  mod <- DMRnet(X, y, algorithm="glamer", family=family)
+  pred <- predict(mod, newx=X, df=3)
+  MSEA <- sum((y - pred)^2)
+  cat("Scenario A: ", MSEA, "\n")
 
-cat("GLAMER\n")
-X <- matrix(sample(1:2,750, replace=TRUE), ncol=15)
-for (i in 1:13)
-  X[,i] <- rnorm(50)
-b_vector <- sample(1:10,2, replace=TRUE)
-y <- X[,14:15] %*% matrix(b_vector)
 
-X<-data.frame(X)
-for (i in 14:15)
-  X[,i] <- factor(X[,i])
+  ######### SCENARIO B GLAMER
 
-mod <- DMRnet(X, y, algorithm="glamer")
-pred <- predict(mod, newx=X, df=3)
-MSEA <- sum((y - pred)^2)
-cat("Scenario A: ", MSEA, "\n")
+  X <- matrix(sample(1:2,750, replace=TRUE), ncol=15)
+  for (i in 3:15)
+    X[,i] <- rnorm(50)
+  b_vector <- sample(1:10,2, replace=TRUE)
+  y <- X[,1:2] %*% matrix(b_vector)
+  if (family == "binomial")
+    y <- factor(y>mean(y))
 
+  X<-data.frame(X)
+  for (i in 1:2)
+    X[,i] <- factor(X[,i])
 
-######### SCENARIO B GLAMER
+  mod <- DMRnet(X, y, algorithm="glamer", family=family)
+  pred <- predict(mod, newx=X, df=3)
+  MSEB <- sum((y - pred)^2)
+  cat("Scenario B: ", MSEB, "\n")
 
-X <- matrix(sample(1:2,750, replace=TRUE), ncol=15)
-for (i in 3:15)
-  X[,i] <- rnorm(50)
-b_vector <- sample(1:10,2, replace=TRUE)
-y <- X[,1:2] %*% matrix(b_vector)
 
-X<-data.frame(X)
-for (i in 1:2)
-  X[,i] <- factor(X[,i])
+  if (stopper)
+    if (MSEA / MSEB > 1000.0 | MSEA > 1e-10 | MSEB > 1e-10)
+      stop(paste("Incorrect compuration for permuted columns: MSE in scenario A =", MSEA, "while MSE in scenario B =", MSEB))
 
-mod <- DMRnet(X, y, algorithm="glamer")
-pred <- predict(mod, newx=X, df=3)
-MSEB <- sum((y - pred)^2)
-cat("Scenario B: ", MSEB, "\n")
+}
 
-
-if (stopper)
-  if (MSEA / MSEB > 1000.0 | MSEA > 1e-10 | MSEB > 1e-10)
-    stop(paste("Incorrect compuration for permuted columns: MSE in scenario A =", MSEA, "while MSE in scenario B =", MSEB))
-
-
+execute("gaussian")
+execute("binomial")
 
 cat("completed\n")
