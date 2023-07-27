1a80cfd591050e3383d2b53e399fca9d54debfc3
SzymonNowakowski <szymon.nowakowski@gmail.com>
2023-07-27 10:27:55 +0200

    automation added

---
+++ all_tests_runner.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+rm simrun
+rm hc_*
+rm mass_???
+rm ms??
+
+Rscript hard_case_CV_airbnb.R &> hc_Ca &
+Rscript hard_case_DMRnet_insurance.R &> hc_Di &
+Rscript hard_case_DMRnet_simulations.R &> hc_Ds &
+Rscript hard_case_GLAMER_promoter.R &> hc_Gp &
+Rscript hard_case_LAPACK_SVD_insurance.R &> hc_LSi &
+Rscript hard_case_NA_insurance.R &> hc_Ni &
+Rscript hard_case_permute_columns.R &> hc_pc &
+Rscript hard_case_SOSnet.R &> hc_S &
+
+Rscript massive_adult.R &> mass_adu &
+Rscript massive_antigua.R &> mass_ant &
+Rscript massive_insurance.R &> mass_ins &
+Rscript massive_promoter.R &> mass_pro &
+
+./simmulation_runner.sh &> simrun &
---
+++ tag_saver.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+
+echo "============================================================" > tag_text
+echo "hard_case_CV_airbnb.R" >> tag_text
+echo "============================================================" >> tag_text
+cat hc_Ca >> tag_text
+
+echo "============================================================" >> tag_text
+echo "hard_case_DMRnet_insurance.R" >> tag_text
+echo "============================================================" >> tag_text
+cat hc_Di >> tag_text
+
+echo "============================================================" >> tag_text
+echo "hard_case_DMRnet_simulations.R" >> tag_text
+echo "============================================================" >> tag_text
+cat hc_Ds >> tag_text
+
+echo "============================================================" >> tag_text
+echo "hard_case_GLAMER_promoter.R" >> tag_text
+echo "============================================================" >> tag_text
+cat hc_Gp >> tag_text
+
+echo "============================================================" >> tag_text
+echo "hard_case_LAPACK_SVD_insurance.R" >> tag_text
+echo "============================================================" >> tag_text
+cat hc_LSi >> tag_text
+
+echo "============================================================" >> tag_text
+echo "hard_case_permute_columns.R" >> tag_text
+echo "============================================================" >> tag_text
+cat hc_pc >> tag_text
+
+echo "============================================================" >> tag_text
+echo "hard_case_SOSnet.R" >> tag_text
+echo "============================================================" >> tag_text
+cat hc_S >> tag_text
+
+
