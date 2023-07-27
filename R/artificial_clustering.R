bd2e0356deec961fee0bbe7bc0dc9d87e6346f27
SzymonNowakowski <szymon.nowakowski@gmail.com>
2023-07-27 12:20:07 +0200

    the work on variable selection started in this file

---
+++ R/artificial_clustering.R
@@ -0,0 +1,6 @@
+artificial_clustering <- function(points) {
+  # it will mimic hclust results, but it will put all points into the same cluster with height equal to max(abs(points))
+
+ l_infty <- max(abs(c(0, points)))
+
+}
