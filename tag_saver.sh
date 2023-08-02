7bf5e4e07cd426750859123174a95ec87f4c8a0f
SzymonNowakowski <szymon.nowakowski@gmail.com>
2023-08-02 14:31:41 +0200

    in tagging lines with a # get treated as comments

--- tag_saver.sh
+++ tag_saver.sh
@@ -2,7 +2,7 @@
 
 echo "" > tag_text
 echo "" >> tag_text
-echo "# The following correctness tests were executed and completed:" >> tag_text
+echo ">> The following correctness tests were executed and completed:" >> tag_text
 echo "" >> tag_text
 echo " - hard_case_CV_airbnb.R" >> tag_text
 echo " - hard_case_DMRnet_insurance.R" >> tag_text
@@ -13,7 +13,7 @@ echo " - hard_case_NA_insurance.R" >> tag_text
 echo " - hard_case_permute_columns.R" >> tag_text
 echo " - hard_case_SOSnet.R" >> tag_text
 echo "" >> tag_text 
-echo "# The following consistency tests were executed and the results were satisfactory:" >> tag_text
+echo ">> The following consistency tests were executed and the results were satisfactory:" >> tag_text
 echo "" >> tag_text  
 echo " - massive_adult.R" >> tag_text
 echo " - massive_antigua.R" >> tag_text
@@ -21,7 +21,7 @@ echo " - massive_insurance.R (40 repeats only)" >> tag_text
 echo " - massive_promoter.R" >> tag_text
 echo " - massive_simulations.R" >> tag_text
 echo "" >> tag_text
-echo "# The correctness tests had the following output:" >> tag_text
+echo ">> The correctness tests had the following output:" >> tag_text
 echo "" >> tag_text
 echo "============================================================" >> tag_text
 echo "hard_case_CV_airbnb.R" >> tag_text
