74420290ca1162b94162cf833fe4d26f825cff0c
SzymonNowakowski <szymon.nowakowski@gmail.com>
2025-08-13 12:50:06 +0200

    results for 0.4.0.9002 show decrease or no-change to error and increased model size for .1se computations

--- simulation_runner.sh
+++ simulation_runner.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-max=19
+max=16
 for beta in 1 2 3 4 5 6; do
   for snr in 1 2 3 4 5 6 7; do
     while [ `ps axu | grep R/bin/exec/R |wc -l` -gt $max ] ; do
-      sleep 60
       echo `ps axu | grep R/bin/exec/R |wc -l`
     done
     echo $beta $snr
     Rscript massive_simulations.R  $beta $snr &> ms$beta$snr &
-    sleep 60   #to give the Rscript time to get invoked
+    sleep 10   #to give the Rscript time to get invoked
   done
 done
