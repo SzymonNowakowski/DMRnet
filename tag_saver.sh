fb9931b7a687c3963e1b20fc653b0d65e3654a70
SzymonNowakowski <szymon.nowakowski@gmail.com>
2025-08-14 13:13:55 +0200

    git adds a colon and a space. Experimenting with something nontypical

--- tag_saver.sh
+++ tag_saver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "" > tag_text
+echo "-test results- :" > tag_text
 echo ">> The following correctness tests were executed and completed:" >> tag_text
 echo "" >> tag_text
 echo " - hard_case_CV_airbnb.R" >> tag_text
